fcbc1411f1b117a32177f44cbe532fb734025e1f
zhouzoro <zyol@outlook.com>
2016-02-22 23:42:07 +0800

    under

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+gulp.task('styles', function() {
+    return sass('public/stylesheets/*.sass', {
+            style: 'expanded'
+        })
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('public/stylesheets'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('public/stylesheets'))
+        .pipe(notify({
+            message: 'Styles task complete.'
+        }));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('public/javascripts/raw/*.js')
+        //.pipe(jshint('.jshintrc'))
+        //.pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('public/javascripts'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/javascripts'))
+        .pipe(notify({
+            message: 'Scripts task complete.'
+        }));
+});
+
+gulp.task('images', function() {
+    return gulp.src('public/images/**/*')
+        .pipe(cache(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('public/images'))
+        .pipe(notify({
+            message: 'Images task complete'
+        }));
+});
+gulp.task('person', function() {
+    return gulp.src('public/stylesheets/person.css')
+         .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('public/stylesheets'))
+        .pipe(notify({
+            message: 'Styles task complete.'
+        }));
+});
+gulp.task('clean', function() {
+    return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
+});
+
+gulp.task('watch', function() {
+
+  // Watch .sass files
+  gulp.watch('public/stylesheets/*.sass', ['styles']);
+  // Watch person.css
+  gulp.watch('public/stylesheets/person.css', ['person']);
+
+  // Watch .js files
+  gulp.watch('public/javascripts/raw/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('public/images/*', ['images']);
+
+});
+
+gulp.task('watch-styles', function() {
+
+  // Watch .scss files
+  gulp.watch('public/stylesheets/*.sass', ['styles']);
+
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,79 @@
+var person = {
+    basics: {
+        name: "",
+        label: "Programmer",
+        picture: "url",
+        email: "",
+        phone: "",
+        url: "rh.org",
+        summary: "Write a short 2-3 sentence biography about yourself",
+        location: {
+            "address": "",
+            "postalCode": "",
+            "city": "",
+            "region": ""
+        },
+        profiles: [{
+            network: "string e.g. Facebook or Twitter",
+            username: "",
+            url: ""
+        }]
+    },
+    work: [{
+        company: "",
+        position: "",
+        url: "",
+        startDate: "",
+        endDate: "",
+        summary: "",
+        highlights: []
+    }],
+    volunteer: [{
+        organization: "",
+        position: "",
+        url: "",
+        startDate: "",
+        endDate: "",
+        summary: "",
+        highlights: []
+    }],
+    education: [{
+        institution: "",
+        area: "",
+        studyType: "",
+        startDate: "",
+        endDate: "",
+        gpa: "",
+        courses: []
+    }],
+    awards: [{
+        title: "",
+        date: "",
+        awarder: "",
+        summary: ""
+    }],
+    publications: [{
+        name: "",
+        publisher: "",
+        releaseDate: "",
+        url: "",
+        summary: ""
+    }],
+    skills: [{
+        name: "",
+        level: "",
+        keywords: []
+    }],
+    languages: [{
+        name: "",
+        level: ""
+    }],
+    interests: [{
+        name: "",
+        keywords: []
+    }],
+    references: [{
+        name: "",
+        reference: ""
+    }]
+}
