510664091712a8bc761ea41ec50889e00cabbd89
zhouzoro <zyol@outlook.com>
2016-02-23 02:04:13 +0800

    failed to sleep early again

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var fs = require('fs-extra')
+    /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/person', function(req, res, next) {
+
+    fs.readFile(__dirname + '/../public/files/person/rh.json', 'utf8', (err, data) => {
+        console.log(__dirname);
+        var person = JSON.parse(data);
+        res.render('person', { person: person });
+    })
 });
 
 module.exports = router;
